34af033bc08484446945ad4476d6dcd6fcd2cec3
thatoneaiguy <animations.by.alby@gmail.com>
2026-05-11 21:15:51 +0100

    setup mod files

---
+++ src/main/java/com/peak/florality/Florality.java
@@ -0,0 +1,10 @@
+package com.peak.florality;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Florality implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
---
+++ src/main/java/com/peak/florality/client/FloralityClient.java
@@ -0,0 +1,10 @@
+package com.peak.florality.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FloralityClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
